af8afb51456df2cceee32e206035f5b16e770294
Jasper Helmich <jasper.helmich@gmail.com>
2019-05-20 16:24:35 +0200

    Added meta tag for robots

--- src/stubs/views/layouts/app.blade.php
+++ src/stubs/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Mach3Builders') }}</title>
+    <meta name="robots" content="noindex, nofollow">	
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
