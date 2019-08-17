888588293496df6c3d0b381a3b46bca1a1c62829
Paul Kruijt <pkruijt@mach3builders.nl>
2019-08-17 08:11:16 +0200

    New setup for UI 1.3.3

--- src/stubs/views/layouts/app.blade.php
+++ src/stubs/views/layouts/app.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Mach3Builders') }}</title>
-    <meta name="robots" content="noindex, nofollow">	
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<title>{{ config('app.name', 'Mach3Builders') }}</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="robots" content="noindex, nofollow">	
+<script src="{{ asset('js/app.js') }}" defer></script>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="{{ Auth::check() ? '' : 'ui-overflow-auto' }}">
     <div id="app" class="ui-layout">
@@ -88,7 +88,6 @@
                     </div>
                 @endif
             </div>
-
 
             <!-- Main view -->
             <main class="ui-layout-view-main">
